9aca7277807ac2492b7f2585a92ddc90cdb73760
amyruth <anrutherford@gmail.com>
2018-06-01 12:45:18 -0400

    added  check for matched card

--- js/app.js
+++ js/app.js
@@ -4,12 +4,13 @@ let resetButton = document.querySelector('.restart');
 let openCards = [];
 let frag = document.createDocumentFragment();
 let moveCounter = 0;
-let cardCounter = 0;
 let moves = document.querySelector('.moves');
-let matches = 0;
-let starCounter = 0;
+let matches = 0; //use for the win condition?
 let starsRemaining = 3;
 let stars = document.querySelector('.stars');
+let time = document.querySelector('.time');
+let minutes = 0;
+let seconds = 0;
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -40,35 +41,39 @@ function makeNewCards(cardDeck) {
 };
 
 function compareCards(cardList) {
-	console.log(cardList);
 	let search = cardList[0].innerHTML;
-	if(cardList[1].innerHTML.indexOf(search) !== -1){
-		cardList.forEach(function(card){
-			card.classList.add('match');		
+	if (cardList[1].innerHTML.indexOf(search) !== -1) {
+		cardList.forEach(function (card) {
+			card.classList.add('match');
 			console.log('match');
 		});
+		matches++;
 		cardList.length = 0;
-	}else{
-		cardList.forEach(function(card){
+		console.log(`matches ${matches}`);
+	} else {
+		cardList.forEach(function (card) {
 			card.classList.remove('show', 'open');
 			console.log('no match');
 		});
 		cardList.length = 0;
 	}
+	if(matches === 8){
+		return console.log('game complete');
+		// launch modal
+	}
 };
 
-function starRating(moveCount){
+function starRating(moveCount) {
 	let star = document.querySelector('.stars');
-	//find way to remove stars and put them back efficiently
 	star.firstElementChild.remove();
 	starsRemaining--;
 	return starsRemaining;
 };
 
-function replaceStars(){
+function replaceStars() {
 	let frag2 = document.createDocumentFragment();
 	let starCount = stars.querySelectorAll('li').length;
-	for(let i = starCount; i <= 2; i++){
+	for (let i = starCount; i <= 2; i++) {
 		let li = document.createElement('li');
 		let starIcon = document.createElement('i');
 		starIcon.classList.add('fa', 'fa-star');
@@ -78,48 +83,66 @@ function replaceStars(){
 	stars.appendChild(frag2);
 }
 
-function displayMoves(cardlist){
-	if(cardlist.length === 2){
+//2 open cards = 1 move
+function displayMoves(cardlist) {
+	if (cardlist.length === 2) {
 		moveCounter += 1;
 		moves.textContent = moveCounter;
 	}
-	if(moveCounter === 8 || moveCounter === 16){
+	if (moveCounter === 8 || moveCounter === 16) {
 		starRating(moveCounter);
 	}
 };
 
-
-
-
-
 function captureCards(card, cardList) {
-	if (cardList.length < 3) {
+	if (cardList.length <= 2 && !card.classList.contains('match')) {
 		card.classList.add('show', 'open');
 		cardList.push(card);
-		console.log(cardList);
+		console.log(cardList[0].childNodes);
 	}
 
-	if(cardList.length === 2){
+	if (cardList.length === 2) {
 		displayMoves(cardList);
 		console.log(`moves: ${moveCounter}`);
-		setTimeout(function(){
-			compareCards(cardList);			
-		} , 700);
+		console.log(cardList[1].childNodes);
+		setTimeout(function () {
+			compareCards(cardList);
+		}, 800);
 	}
 };
 
-function cardListener(deck,cardHand){
+function cardListener(deck, cardHand) {
 	deck.querySelectorAll('li').forEach(function (card) {
 		console.log('assigning listeners');
 		card.addEventListener('click', function () {
-			if(openCards.length === 2){
+			console.log('clicked card');
+			if (openCards.length === 2) {
 				return;
-			}else{
+			} else {
 				let clickedCard = this;
 				captureCards(clickedCard, openCards);
 			}
 		});
-	}, false);	
+	}, false);
+};
+
+// true/false flag for timer operation?
+
+function gameTime(){
+	seconds++;
+	time.innerHTML = `${minutes} min ${seconds} sec`;
+	console.log(`interval: ${seconds}`);
+	if(seconds === (1000*60/1000)){
+		minutes++;
+		seconds = 1;
+	}	
+};
+
+function stopTime(interval){
+	clearInterval(interval);
+	minutes = 0;
+	seconds = 0;
+	console.log(`cleared interval ${seconds}`);
 };
 
 function newBoard() {
@@ -133,19 +156,25 @@ function newBoard() {
 	deck.appendChild(frag);
 	replaceStars();
 	console.log(deck);
-	cardListener(deck, openCards);	
+	cardListener(deck, openCards);
 };
 
-//make a click handler for the reset button
+
+
+//click handler for the reset button
 resetButton.addEventListener('click', function () {
-	if(openCards.length !== 0){
+	
+	if (openCards.length !== 0) {
 		openCards.length = 0;
 	}
 	newBoard();
 });
 
 //when page loads for the first time a new board is created
+
 newBoard();
+
+
 
 
 
